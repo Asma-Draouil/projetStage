e86a98f067dc360f35b32341e7bc9590dc1d3b07
Asma Draouil <asmadraouil2@gmail.com>
2024-08-07 22:48:49 +0100

    modif

--- src/main/java/com/projetStage/controllers/utilisateurs/EtudiantController.java
+++ src/main/java/com/projetStage/controllers/utilisateurs/EtudiantController.java
@@ -7,9 +7,11 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.projetStage.models.dto.utilisateurs.EtudiantDto;
 import com.projetStage.services.utilisateurs.EtudiantService;
 
@@ -46,4 +48,27 @@ public class EtudiantController {
 		return ResponseEntity.ok().build();
 	}
 	
+	@PutMapping("/{idEtudiant}/cours/{}idCours")
+	public ResponseEntity<EtudiantDto> assignCoursToEtudiant(
+			@PathVariable Integer idEtudiant,
+			@PathVariable Integer idCours
+			){
+		return ResponseEntity.ok(etudiantService.assignCoursToEtudiant(idEtudiant, idCours));
+	}
+	
+	@PutMapping("/{idEtudiant}/certificat/{}idCertificat")
+	public ResponseEntity<EtudiantDto> assignCertifToEtudiant(
+			@PathVariable Integer idEtudiant,
+			@PathVariable Integer idCertificat
+			){
+		return ResponseEntity.ok(etudiantService.assignCoursToEtudiant(idEtudiant, idCertificat));
+	}
+	
+	@PutMapping("/{idEtudiant}/paiement/{}idPaiement")
+	public ResponseEntity<EtudiantDto> assignPaiementToEtudiant(
+			@PathVariable Integer idEtudiant,
+			@PathVariable Integer idPaiement
+			){
+		return ResponseEntity.ok(etudiantService.assignCoursToEtudiant(idEtudiant, idPaiement));
+	}
 }
